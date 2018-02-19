021bea5e8bc34c8d886378ecd37cf94460f62e7a
Claude Janssen <35732829+ClaudeJanssenPro@users.noreply.github.com>
2018-02-19 11:45:45 +0100

    comment+

--- contenu.php
+++ contenu.php
@@ -1,2 +1,11 @@
+<!--
+- écran 2 :
+la liste des tâches à faire, avec pour chaque tâche, une checkbox.
+Lorsqu'une tâche est effectuée, on coche la tâche puis on appuye sur un bouton "Enregistrer"
+qui rafraichit la liste en barrant la tâche terminée et en la mettant dans la zone "archivée".
+- Fichier "contenu.php" :
+il lit le contenu du fichier json, et affiche chaque entrée dans la bonne zone ("A Faire" ou "Archive")
+avec le contenu html nécessaire pour avoir une checkbox.
+-->
 <?php
 ?>
