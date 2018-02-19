f4c17077a9e513a2b5f88246d546c09d8399c851
Claude Janssen <35732829+ClaudeJanssenPro@users.noreply.github.com>
2018-02-19 11:47:29 +0100

    comment+

--- formulaire.php
+++ formulaire.php
@@ -1,3 +1,9 @@
-<!-- écran 1 : un petit formulaire permettant d'ajouter une tâche (un champ "textarea" et le bouton "submit") -->
+<!--
+- écran 1 :
+un petit formulaire permettant d'ajouter une tâche (un champ "textarea" et le bouton "submit")
+- Fichier "formulaire.php" :
+Lorsqu'on traite le formulaire il faut, après sanitization et validation,
+stocker les tâches au format JSON dans un fichier TXT ( par exemple todo.json)
+-->
 <?php
 ?>
