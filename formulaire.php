bb76d633fede1b7060a730ad52d90ff048bb2ee3
ClaudeJanssenPro <claudejanssen.pro@gmail.com>
2018-02-20 16:13:32 +0100

    Sanitized

--- formulaire.php
+++ formulaire.php
@@ -7,34 +7,7 @@ stocker les tâches au format JSON dans un fichier TXT ( par exemple todo.json)
 http://coursesweb.net/php-mysql/add-form-data-text-file-json-format_t
 -->
 <?php
-// function isExist($var){  //vérifie si la variable existe  +ajouter dans input value="<?= isExist($name); /?/>"  la rajoute automatiquement dans le champ quand refresh page
-//   if(isset($var)){
-//     echo $var;
-//   };
-// }
-// // and the LOCK_EX flag to prevent anyone else writing to the file at the same time
-// // file_put_contents($file, $add_task, LOCK_EX);
-// if (isset($_POST['submit'])){
-//     $options=array(
-//               "add_task" => FILTER_SANITIZE_STRING,
-//                   );
-//     $result=filter_input_array(INPUT_POST, $options);
-//     $checkResult=[];
-//     }
-// //     $result["subject"]=$checkResult;  //affiche le contenu du subject
-// //      print_r($result);
-//     $add_task=trim($result["add_task"]);  //trim oblige à insérer un caractère, et pas des espaces dans le formulaire
-// //conditions si champs existe et champs bien remplis
-//     if(isset($add_task) AND !empty($add_task)) {
-//       $verif_add_task="ok";
-//     }
-//     else{
-//       $verif_add_task="ko";
-// FILE_APPEND |
-//     }
 $file = 'todo.json';
-// $add_task = "add_task";
-// $jsonaddtask = json_encode($add_task, JSON_PRETTY_PRINT);
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -45,7 +18,6 @@ $file = 'todo.json';
     <title>Todolist - Formulaire</title>
   </head>
   <body>
-    <!-- <form action="save_json.php" method="post"> -->
     <form method="post">
       <p>
         <label for="add_task">Ajouter une tâche</label></p>
@@ -56,8 +28,13 @@ $file = 'todo.json';
         <button type="submit" name="submit">
           <?php
           $add_task = $_POST["add_task"];
-          $jsonaddtask = json_encode($add_task, JSON_PRETTY_PRINT);
-          file_put_contents($file, $jsonaddtask, LOCK_EX);
+          $san_add_task = filter_var($add_task, FILTER_SANITIZE_STRING);
+          if(isset($add_task) AND !empty($add_task)) {
+            $jsonaddtask = json_encode($san_add_task, JSON_PRETTY_PRINT);
+            file_put_contents($file, $jsonaddtask, FILE_APPEND |LOCK_EX);          }
+          else{
+            echo "Ajoute une nouvelle tâche, WIP";
+          }
           ?> Envoi
         </button>
       </p>
