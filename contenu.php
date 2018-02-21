f7dfae88098294d12cd8954bbc5735c6041402dd
ClaudeJanssenPro <claudejanssen.pro@gmail.com>
2018-02-21 13:46:40 +0100

    1346

--- contenu.php
+++ contenu.php
@@ -8,4 +8,27 @@ il lit le contenu du fichier json, et affiche chaque entrée dans la bonne zone 
 avec le contenu html nécessaire pour avoir une checkbox.
 -->
 <?php
+
+// $add_task = $_POST["add_task"];
+// $san_add_task = filter_var($add_task, FILTER_SANITIZE_STRING);
+// if(isset($add_task) AND !empty($add_task)) {
+//   $jsonaddtask = json_encode($san_add_task, JSON_PRETTY_PRINT);
+//   file_put_contents($file, $jsonaddtask, FILE_APPEND |LOCK_EX);
+// }
+
+$file = file_get_contents('todo.json');
+$json = json_decode($file, true); // decode the JSON into an associative array
+// var_dump($json);
+echo $json;
+//
+// This will print out the contents of the array in a nice readable format. Then, you access the elements you want, like so:
+//
+// $temperatureMin = $json['daily']['data'][0]['temperatureMin'];
+// $temperatureMax = $json['daily']['data'][0]['temperatureMax'];
+//
+// Or loop through the array however you wish:
+//
+// foreach ($json['daily']['data'] as $field => $value) {
+//     // Use $field and $value here
+// }
 ?>
--- formulaire.php
+++ formulaire.php
@@ -5,15 +5,19 @@ un petit formulaire permettant d'ajouter une tâche (un champ "textarea" et le b
 Lorsqu'on traite le formulaire il faut, après sanitization [???] et validation [???],
 stocker les tâches au format JSON dans un fichier TXT ( par exemple todo.json)
 http://coursesweb.net/php-mysql/add-form-data-text-file-json-format_t
+file_put_contents($file, $jsonaddtask, FILE_APPEND |LOCK_EX);
 -->
 <?php
-$file = 'todo.json';
-$add_task = $_POST["add_task"];
-$san_add_task = filter_var($add_task, FILTER_SANITIZE_STRING);
-if(isset($add_task) AND !empty($add_task)) {
-  $jsonaddtask = json_encode($san_add_task, JSON_PRETTY_PRINT);
-  file_put_contents($file, $jsonaddtask, FILE_APPEND |LOCK_EX);
+$file = file_get_contents('todo.json');
+$json = json_decode($file, true); // decode the JSON into an associative array
+$add_task = $_POST ['add_task'];
+if(isset($add_task)) {
+  $san_add_task = filter_var($add_task, FILTER_SANITIZE_STRING);
+
 }
+$jsonaddtask = json_encode($san_add_task, JSON_FORCE_OBJECT);
+file_put_contents($file, $jsonaddtask, LOCK_EX);
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -21,20 +25,27 @@ if(isset($add_task) AND !empty($add_task)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Athiti" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+		<link rel="stylesheet" href="style.css" type="text/css" charset="utf-8" />
     <title>Todolist - Formulaire</title>
   </head>
   <body>
+  <div class="container">
     <form method="post">
+      <fieldset>
       <p>
-        <label for="add_task">Ajouter une tâche</label></p>
+        <label for="new_task">Ajouter une tâche</label></p>
       <p>
         <textarea name="add_task" id="new_task" placeholder="(ajouter ta tâche ici)" class="expanding" required autofocus></textarea>
       </p>
       <p>
         <button type="submit" name="submit">
-          Envoi
+          Ajouter
         </button>
       </p>
+      </fieldset>
     </form>
-    </body>
+  </div>
+  </body>
 </html>
