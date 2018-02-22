b3e09ee3b8c70f29051ee9d013d15a9295c7314f
KevinCharlier <kevcharlier@gmail.com>
2018-02-22 16:06:12 +0100

    test

--- contenu.php
+++ contenu.php
@@ -1,83 +1,107 @@
-<!--
-- écran 2 :
-la liste des tâches à faire, avec pour chaque tâche, une checkbox.
-Lorsqu'une tâche est effectuée, on coche la tâche puis on appuye sur un bouton "Enregistrer"
-qui rafraichit la liste en barrant la tâche terminée et en la mettant dans la zone "archivée".
-- Fichier "contenu.php" :
-il lit le contenu du fichier json, et affiche chaque entrée dans la bonne zone ("A Faire" ou "Archive")
-avec le contenu html nécessaire pour avoir une checkbox.
--->
 <?php
-
-// $add_task = $_POST["add_task"];
-// $san_add_task = filter_var($add_task, FILTER_SANITIZE_STRING);
-// if(isset($add_task) AND !empty($add_task)) {
-//   $jsonaddtask = json_encode($san_add_task, JSON_PRETTY_PRINT);
-//   file_put_contents($file, $jsonaddtask, FILE_APPEND |LOCK_EX);
-// }
-
-// $file = file_get_contents('todo.json');
-// $json = json_decode($file, true); // decode the JSON into an associative array
-// // var_dump($json);
-// echo $json;
-
-$contenu_fichier_json = file_get_contents('./todo.json');
-$receipt = json_decode($contenu_fichier_json, true);
-// print_r($receipt);
-//
-// This will print out the contents of the array in a nice readable format. Then, you access the elements you want, like so:
-//
-// $temperatureMin = $json['daily']['data'][0]['temperatureMin'];
-// $temperatureMax = $json['daily']['data'][0]['temperatureMax'];
-//
-// Or loop through the array however you wish:
-//
-// foreach ($json['daily']['data'] as $field => $value) {
-//     // Use $field and $value here
-// }
+    ini_set("dispay_errors",0);error_reporting(0);
+    $filename="todo.json";
+    $content_json = file_get_contents($filename);
+    $receipt = json_decode($content_json, true);
+    
+    // bouton ajouter
+    if (isset($_POST['submit']) AND end($receipt)['taskname'] != $_POST['newtask']){
+        $add_task = $_POST['newtask'];
+        $array_task = array("taskname" => $add_task,
+                             "Terminer" => false);
+        $receipt[] = $array_task;
+        $json= json_encode($receipt, JSON_PRETTY_PRINT);
+        file_put_contents($filename, $json);
+        $receipt = json_decode($json, true);
+    }
+    
+    // bouton enregistrer
+    if (isset($_POST['save'])){
+        $choice=$_POST['newtask'];
+        for ($init = 0; $init < count($receipt); $init ++){
+            if (in_array($receipt[$init]['taskname'], $choice)){
+              $receipt[$init]['Terminer'] = true;
+            }
+        }
+        $json= json_encode($receipt, JSON_PRETTY_PRINT);
+        file_put_contents($filename, $json);
+        $receipt = json_decode($json, true);
+    }
+    // bouton retirer
+    if (isset($_POST['unsave'])){
+        $choice=$_POST['removetask'];
+        // var_dump($choice);
+        for ($init = 0; $init < count($receipt); $init ++){
+            if (!in_array($receipt[$init]['taskname'], $choice)){
+              $receipt[$init]['Terminer'] = false;
+            }
+        }
+        $json= json_encode($receipt, JSON_PRETTY_PRINT);
+        file_put_contents($filename, $json);
+        $receipt = json_decode($json, true);
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="stylesheet" href="style.css" type="text/css" charset="utf-8" />
-<title>To do</title>
-</head>
-<body>
-<div class="container">
-<form action="" method="POST" name="formulaire" id="formulaire">
-<fieldset>
-<h3>A faire</h3>
-<ul id="incomplete-tasks">
-<?php
-foreach ($receipt as $key => $value) {
-  if ($value["Terminer"] == false){
-    echo '<li><input type="checkbox" name="todo" value="Nom"><label>'.$value["Nom"].'</label>'.'<input type="text"></li>'.'<br/>';
-  }
-  else {
-    echo '';
-  }
-}
- ?>
-</ul>
-<button name="submit" type="submit" form="formulaire">Enregistrer</button>
-<h3>Archive</h3>
-<ul id="completed-tasks">
-<?php
-foreach ($receipt as $key => $value) {
-  if ($value["Terminer"] == true){
-    echo '<li><input type="checkbox" name="archive" value="Nom" checked><label>'.$value["Nom"].'</label>'.'<input type="text"></li>'.'<br/>';
-  }
-  else {
-    echo '';
-  }
-}
- ?>
-</ul>
-</fieldset>
-</form>
-</div>
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<link rel="stylesheet" href="style.css" type="text/css" charset="utf-8" />
+		<title>To do</title>
+	</head>
+    <body>
+            <div class="container">
+            <fieldset>
+                <form action="contenu.php" method="POST">
+                        
+                        <h3>A faire</h3>
+                        <ul id="incomplete-tasks">
+                <?php 
+                    foreach ($receipt as $key => $value) {  
+                        if ($value["Terminer"] == false){
+
+                            echo "<li><input type='checkbox' name='newtask[]' value='".$value["taskname"]."'/>
+                                <label for='choice'>".$value["taskname"]."</label></li><br />"; 
+                        }
+                    } 
+                ?>
+                        </ul>
+                        <button class="save" name="save" type="submit">Enregistrer</button>
+                </form>
+                <form action="contenu.php" method="POST">
+
+                        <h3>Archive</h3>
+                        <ul id="completed-tasks">
+                <?php 
+                    foreach ($receipt as $key => $value) {  
+                        if ($value["Terminer"] == true){
+                            echo "<li><input type='checkbox' name='removetask[]' value='".$value["taskname"]."'checked/>
+                                <label for='choice'>".$value["taskname"]."</label></li><br />";  
+                        }
+                    } 
+                ?>
+                        </ul>
+                            <button class="unsave" name="unsave" type="submit">Retirer</button>
+                </form>
+            </fieldset>
+            </div>
+
+            <div class="container">
+            <fieldset>
+                <form method="post" action="contenu.php">
+                        <p>
+                            <h3>Ajouter une nouvelle tache</h3>
+                        </p>
+                        <p>
+                            <textarea name="newtask" placeholder="(ajouter ta tâche ici)" class="expanding" autofocus></textarea>
+                        </p>
+                        <p>
+                            <button type="submit" name="submit">Ajouter</button>
+                        </p>
+                </form>
+            </fieldset>
+            </div>
+        </fieldset>
+    </body>
 </html>
--- formulaire.php
+++ formulaire.php
@@ -1,63 +1,45 @@
-<!--
-- écran 1 :
-un petit formulaire permettant d'ajouter une tâche (un champ "textarea" et le bouton "submit")
-- Fichier "formulaire.php" :
-Lorsqu'on traite le formulaire il faut, après sanitization [???] et validation [???],
-stocker les tâches au format JSON dans un fichier TXT ( par exemple todo.json)
-http://coursesweb.net/php-mysql/add-form-data-text-file-json-format_t
-file_put_contents($file, $jsonaddtask, FILE_APPEND |LOCK_EX);
--->
-<?php
-if(isset($_POST['submit'])){
-
-  $options = array(
-    'add_task' => FILTER_SANITIZE_STRING
-  );
-
-  $result = filter_input_array(INPUT_POST, $options);
-  $checkresult =[];
-
+<?php 
+if(isset($_POST['submit'])){  
+  $options = array('add_task' => FILTER_SANITIZE_STRING  );  
+  $result = filter_input_array(INPUT_POST, $options);  
+  $checkresult =[];  
   $add_task = trim($result['add_task']);
-
 }
-// var_dump($add_task);
 $file = './todo.json';
 $contenu_fichier_json = file_get_contents($file);
 $reception = json_decode($contenu_fichier_json, true);
-// var_dump($reception);
-if(isset($add_task)) {
-  $reception[] = array("Nom" => $add_task, "Terminer" => false);
-  $jsonaddtask = json_encode($reception, JSON_PRETTY_PRINT);
-  file_put_contents($file, $jsonaddtask, LOCK_EX);
-}
+if(isset($add_task)) {  
+  $reception[] = array("Nom" => $add_task, "Terminer" => false);  
+  $jsonaddtask = json_encode($reception, JSON_PRETTY_PRINT);  
+  file_put_contents($file, $jsonaddtask, LOCK_EX);}
 ?>
+  
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css?family=Athiti" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<link href="https://fonts.googleapis.com/css?family=Athiti" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 		<link rel="stylesheet" href="style.css" type="text/css" charset="utf-8" />
-    <title>Todolist - Formulaire</title>
-  </head>
-  <body>
-  <div class="container">
-    <form method="post">
-      <fieldset>
-      <p>
-        <label for="new_task">Ajouter une tâche</label></p>
-      <p>
-        <textarea name="add_task" id="new_task" placeholder="(ajouter ta tâche ici)" class="expanding" required autofocus></textarea>
-      </p>
-      <p>
-        <button type="submit" name="submit">
-          Ajouter
-        </button>
-      </p>
-      </fieldset>
-    </form>
-  </div>
-  </body>
+		<title>Todolist - Formulaire</title>
+	</head>
+	<body>
+		<div class="container">
+			<form method="post">
+				<fieldset>
+					<p>
+						<label for="new_task">Ajouter une tâche</label>
+					</p>
+					<p>
+						<textarea name="add_task" id="new_task" placeholder="(ajouter ta tâche ici)" class="expanding" required autofocus></textarea>
+					</p>
+					<p>
+						<button type="submit" name="submit">Ajouter</button>
+					</p>
+				</fieldset>
+			</form>
+		</div>
+	</body>
 </html>
